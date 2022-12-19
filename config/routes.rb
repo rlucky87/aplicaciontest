682515b36a215285a5d32880273f8e143ae02753
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-12-19 08:32:24 +0100

    add route homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "pages#home"
   resources :restaurants, only: %i[index show] do
     resources :bookings, only: %i[create]
   end
