9de942ae16bae8fbf5e2520756d94567a1108b1b
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-12-19 08:19:08 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: %i[index show] do
+    resources :bookings, only: %i[create]
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
