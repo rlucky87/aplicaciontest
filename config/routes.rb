dcd2697bf5578013f16f0bf306a2559a967abf63
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-12-20 15:00:21 +0100

    heroku end

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :restaurants, only: %i[index show] do
-    #resources :bookings, only: %i[create]
+    resources :bookings, only: %i[create]
   end
 
   # Defines the root path route ("/")
