9f6dff8f9b7862e79b3857c31f551d0fbde9fae1
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-12-24 22:35:42 +0100

    add db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_072100) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_072748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,28 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_072100) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.string "status"
+    t.date "reservation_date"
+    t.bigint "restaurant_id", null: false
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_reservations_on_customer_id"
+    t.index ["restaurant_id"], name: "index_reservations_on_restaurant_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "adress"
+    t.integer "rating"
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_restaurants_on_customer_id"
+  end
+
+  add_foreign_key "reservations", "customers"
+  add_foreign_key "reservations", "restaurants"
+  add_foreign_key "restaurants", "customers"
 end
