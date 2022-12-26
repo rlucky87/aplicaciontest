55e79be124ee9bb44ece5814e43f6ae79916422b
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-12-26 21:21:03 +0100

    erase reference key

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
+  remove_reference :customers, foreign_key: true
   belongs_to :customer
   has_many :reservations
 end
--- db/migrate/20221225222852_remove_customerto_restaurant.rb
+++ db/migrate/20221225222852_remove_customerto_restaurant.rb
@@ -1,5 +1,5 @@
 class RemoveCustomertoRestaurant < ActiveRecord::Migration[7.0]
   def change
-    
+    remove_foreign_key :customers, :restaurants
   end
 end
